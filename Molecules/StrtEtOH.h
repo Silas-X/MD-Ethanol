e9aa853fe9ee3ebdb47e41cecd3caee7c89ab30b
Silas <Silas_X@outlook.com>
2019-11-17 02:10:26 +0800

    update StrtEtOH with a atomTypeNumber

--- Molecules/StrtEtOH.h
+++ Molecules/StrtEtOH.h
@@ -10,7 +10,7 @@ class StrtEtOH : Location
 {
 private:
     Atom *C1, *C2, (*H1)[3], (*H2)[2], *O, *H3;
-    bool Create(Atom *atom,const AtomBase &base, const Location &pos);
+    bool Create(Atom *atom,const int typeNbr,const AtomBase &base, const Location &pos);
     int groupNbr;
 public:
     StrtEtOH(Location pos);
